d39e71d19cf3cf64b788b821022691e27820d828
kkito0726 <k.kito0726@gmail.com>
2023-02-05 06:26:24 +0000

    spring bootアプリ3つをnginxで統合した

---
+++ author-api/src/main/java/com/ken/authorapi/AuthorApiApplication.java
@@ -0,0 +1,13 @@
+package com.ken.authorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorApiApplication.class, args);
+	}
+
+}
