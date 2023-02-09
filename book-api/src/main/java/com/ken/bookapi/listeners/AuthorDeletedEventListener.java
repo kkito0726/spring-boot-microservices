e5ab5493ac304369ba60b205f6c4ab800ee46abd
kkito0726 <k.kito0726@gmail.com>
2023-02-09 04:07:49 +0000

    イベントリスナーの追加

--- book-api/src/main/java/com/ken/bookapi/listeners/AuthorDeletedEventListener.java
+++ book-api/src/main/java/com/ken/bookapi/listeners/AuthorDeletedEventListener.java
@@ -1,15 +1,31 @@
 package com.ken.bookapi.listeners;
 
 import com.ken.bookapi.config.RabbitMQConfig;
+import com.ken.bookapi.mappers.AuthorMapper;
+import com.ken.bookapi.models.Author;
+import com.ken.bookapi.models.Book;
+import com.ken.bookapi.repositories.AuthorRepository;
+import com.ken.bookapi.repositories.BookRepository;
 import com.ken.shared.domein.AuthorEventDto;
 import com.ken.shared.models.CustomMessage;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
+@Transactional
 public class AuthorDeletedEventListener {
+
+  private final AuthorRepository _authorRepository;
+  private final BookRepository _bookRepository;
+  private final AuthorMapper _authorMapper;
 
   @RabbitListener(queues = { RabbitMQConfig.QUEUE_AUTHOR_DELETED })
   public void handleMessage(CustomMessage<AuthorEventDto> message) {
@@ -18,5 +34,27 @@ public class AuthorDeletedEventListener {
       AuthorDeletedEventListener.class,
       message.toString()
     );
+    AuthorEventDto authorEventDto = message.getPayload();
+
+    Optional<Author> existingAuthor = _authorRepository.findById(
+      authorEventDto.getId()
+    );
+    if (existingAuthor.isEmpty()) {
+      return;
+    }
+
+    Author author = _authorMapper.toEntity(authorEventDto);
+    _authorRepository.delete(author);
+
+    List<Book> books = _bookRepository.findAllByAuthors(authorEventDto.getId());
+    for (Book bookItem : books) {
+      List<UUID> filtered = bookItem
+        .getAuthors()
+        .stream()
+        .filter(authorId -> !authorId.equals(authorEventDto.getId()))
+        .toList();
+
+      bookItem.setAuthors((filtered));
+    }
   }
 }
