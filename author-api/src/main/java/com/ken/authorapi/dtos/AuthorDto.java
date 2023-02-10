cf13e53a169badc1b3222b807186b3b0f63b841f
kkito0726 <k.kito0726@gmail.com>
2023-02-10 14:05:36 +0000

    author-apiからbookIdsを取得できるようにした。booksはまだnull

--- author-api/src/main/java/com/ken/authorapi/dtos/AuthorDto.java
+++ author-api/src/main/java/com/ken/authorapi/dtos/AuthorDto.java
@@ -1,6 +1,8 @@
 package com.ken.authorapi.dtos;
 
 import com.ken.shared.models.DtoBase;
+import java.util.List;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -16,4 +18,6 @@ public class AuthorDto extends DtoBase {
 
   private String name;
   private String description;
+  private List<BookDto> books;
+  private List<UUID> bookIds;
 }
